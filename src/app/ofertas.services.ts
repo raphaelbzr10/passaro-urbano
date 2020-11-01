be257a5a4902e3e6ecf757dc064fe4d9e02c453e
Raphael S. Bezerra <raphaelbzr@gmail.com>
2020-11-01 00:28:08 -0300

    rotas e navegação

--- src/app/ofertas.services.ts
+++ src/app/ofertas.services.ts
@@ -3,18 +3,46 @@ import { Injectable } from '@angular/core'
 import 'rxjs/add/operator/toPromise'
 
 import { Oferta } from './shared/oferta.model'
-
+import { URL_API } from './app.api'
 
 @Injectable()
 export class OfertasService {
+    // private url_api = "http://localhost:3000/ofertas"
     constructor(private http: Http) { }
 
     public getOfertas(): Promise<Oferta[]> {
         //efetuar uma requisição http
-        return this.http.get('http://localhost:3000/ofertas?destaque=true')
+        return this.http.get(`${URL_API}/ofertas?destaque=true`)
             .toPromise()
             .then((resposta: any) => resposta.json())
 
         //retornar uma promise Oferta
+    }
+    public getOfertasPorCategoria(categoria: string): Promise<Oferta[]> {
+        return this.http.get(`${URL_API}/ofertas?categoria=${categoria}`)
+            .toPromise()
+            .then((resposta: any) => resposta.json())
+    }
+    public getOfertaPorId(id: number): Promise<Oferta> {
+        return this.http.get(`${URL_API}/ofertas?id=${id}`)
+            .toPromise()
+            .then((resposta: any) => resposta.json()[0])
+    }
+    public getComoUsarOfertaPorId(id: number): Promise<string> {
+        return this.http.get(`${URL_API}/como-usar?id=${id}`)
+            .toPromise()
+            .then((resposta: any) => {
+                
+                return resposta.json()[0].descricao
+            })
+    }
+
+    public getOndeFicaOfertaPorId(id: number): Promise<string> {
+        return this.http.get(`${URL_API}/onde-fica?id=${id}`)
+            .toPromise()
+            .then((resposta: any) => {
+                
+                return resposta.json()[0].descricao
+            })
     }
 }
