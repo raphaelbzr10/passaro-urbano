44486d9b9b62e77462ec740f7098b34fb5313545
Raphael S. Bezerra <raphaelbzr@gmail.com>
2020-11-01 00:24:05 -0300

    serviços e promises

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 
+import { OfertasService } from '../ofertas.services'
+import { Oferta } from '../shared/oferta.model'
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  providers: [OfertasService] //uso dos serviços no Angular
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  public ofertas: Oferta[]
+  constructor(private ofertasService: OfertasService) { }
 
   ngOnInit() {
+    // this.ofertas = this.ofertasService.getOfertas()
+    // console.log(this.ofertas)
+
+    this.ofertasService.getOfertas2()
+      .then((ofertas: Oferta[]) => {
+        console.log('a função resolve() foi resolvida depois de 3 segundos')
+        this.ofertas = ofertas
+      })
+      .catch((param: any) => { 
+        console.log(param) 
+      })
+
+
   }
 
 }
---
+++ src/app/ofertas.services.ts
@@ -0,0 +1,85 @@
+import { Oferta } from './shared/oferta.model'
+export class OfertasService {
+
+    public ofertas: Array<Oferta> = [
+        {
+            id: 1,
+            categoria: "restaurante",
+            titulo: "Super Burger",
+            descricao_oferta: "Rodízio de Mini-hambúrger com opção de entrada.",
+            anunciante: "Original Burger",
+            valor: 29.90,
+            destaque: true,
+            imagens: [
+                { url: "/assets/ofertas/1/img1.jpg" },
+                { url: "/assets/ofertas/1/img2.jpg" },
+                { url: "/assets/ofertas/1/img3.jpg" },
+                { url: "/assets/ofertas/1/img4.jpg" }
+            ]
+        },
+        {
+            id: 2,
+            categoria: "restaurante",
+            titulo: "Cozinha Mexicana",
+            descricao_oferta: "Almoço ou Jantar com Rodízio Mexicano delicioso.",
+            anunciante: "Mexicana",
+            valor: 32.90,
+            destaque: true,
+            imagens: [
+                { url: "/assets/ofertas/2/img1.jpg" },
+                { url: "/assets/ofertas/2/img2.jpg" },
+                { url: "/assets/ofertas/2/img3.jpg" },
+                { url: "/assets/ofertas/2/img4.jpg" }
+            ]
+
+        },
+        {
+            id: 4,
+            categoria: "diversao",
+            titulo: "Estância das águas",
+            descricao_oferta: "Diversão garantida com piscinas, trilhas e muito mais.",
+            anunciante: "Estância das águas",
+            valor: 31.90,
+            destaque: true,
+            imagens: [
+                { url: "/assets/ofertas/3/img1.jpg" },
+                { url: "/assets/ofertas/3/img2.jpg" },
+                { url: "/assets/ofertas/3/img3.jpg" },
+                { url: "/assets/ofertas/3/img4.jpg" },
+                { url: "/assets/ofertas/3/img5.jpg" },
+                { url: "/assets/ofertas/3/img6.jpg" }
+            ]
+        }
+    ]
+    public getOfertas(): Array<Oferta> {
+        return this.ofertas
+    }
+
+    public getOfertas2(): Promise<Array<Oferta>> {
+        return new Promise((resolve, reject) => {
+            //algum tipo de processamento, que ao finalizar, chama a função resolve ou a função reject
+            // console.log('será que passou por aqui?')
+            let deu_certo = true
+            if(deu_certo){
+                setTimeout(() => resolve(this.ofertas), 3000)
+            } else {
+                reject({codigo_erro: 404, mensagem_erro: 'Servidor não encontrado XYZ'})
+            }
+        })
+        .then((ofertas: Oferta[]) => {
+            //fazer alguma tentativa
+            console.log('primeiro then')
+            return ofertas
+        })
+        .then((ofertas: Oferta[]) => {
+            console.log('segundo then')
+            return new Promise((resolve2, reject2) => {
+                setTimeout(() => {resolve2(ofertas)},3000)
+            })
+        })
+        .then((ofertas: Oferta[]) => {
+            console.log('terceiro then executado após 3 segundos porque estava aguardando uma promisse ser resolvida')
+            return ofertas
+        })
+    }
+}
