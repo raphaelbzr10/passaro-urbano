d5ac7ae4f95f9d5202c1863489eb39d1b619d00a
Raphael S. Bezerra <raphael.bezerra@b2wdigital.com>
2020-12-02 10:54:17 -0300

    criado o model ItemCarrinho

--- src/app/carrinho.service.ts
+++ src/app/carrinho.service.ts
@@ -1,5 +1,7 @@
-class CarrinhoService {
+import { ItemCarrinho } from './shared/item-carrinho.model';
 
+class CarrinhoService {
+    public itens: ItemCarrinho[] = [];
 }
 
 export default CarrinhoService;
