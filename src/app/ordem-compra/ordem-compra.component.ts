b6c592a71d213d8514d7d1d9bd9eea068345d7ca
Raphael S. Bezerra <raphael.bezerra@b2wdigital.com>
2020-12-02 11:16:56 -0300

    atribuindo CarrinhoService ao componente OrdemCompra

--- src/app/ordem-compra/ordem-compra.component.ts
+++ src/app/ordem-compra/ordem-compra.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import CarrinhoService from 'app/carrinho.service';
 import { OrdemCompraService } from '../ordem-compra.service'
 import { Pedido } from '../shared/pedido.model'
 
@@ -7,7 +8,7 @@ import { Pedido } from '../shared/pedido.model'
   selector: 'app-ordem-compra',
   templateUrl: './ordem-compra.component.html',
   styleUrls: ['./ordem-compra.component.css'],
-  providers: [ OrdemCompraService ]
+  providers: [ OrdemCompraService, CarrinhoService ]
 })
 export class OrdemCompraComponent implements OnInit {
 
@@ -20,10 +21,10 @@ export class OrdemCompraComponent implements OnInit {
     'formaPagamento': new FormControl(null, [ Validators.required ])
   });
 
-  constructor(private ordemCompraService: OrdemCompraService) { }
+  constructor(private ordemCompraService: OrdemCompraService, private carrinhoService: CarrinhoService) { }
 
   ngOnInit() {
-    
+    console.log(this.carrinhoService.exibirItens());
   }
 
   public confirmarCompra(): void {
