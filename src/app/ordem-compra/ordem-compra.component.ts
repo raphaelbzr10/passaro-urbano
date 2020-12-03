20384efdc120e6a8806c6654abb41f01898ed25e
Raphael S. Bezerra <raphael.bezerra@b2wdigital.com>
2020-12-03 11:47:31 -0300

    impedindo confirmação da compra se ordem compra não possuir itens

--- src/app/ordem-compra/ordem-compra.component.ts
+++ src/app/ordem-compra/ordem-compra.component.ts
@@ -32,26 +32,29 @@ export class OrdemCompraComponent implements OnInit {
   public confirmarCompra(): void {
     if (this.formulario.status === 'INVALID') {
 
-      console.log('formulário está inválido');
-
       this.formulario.get('endereco').markAsTouched();
       this.formulario.get('numero').markAsTouched();
       this.formulario.get('complemento').markAsTouched();
       this.formulario.get('formaPagamento').markAsTouched();
 
     } else {
-      let pedido: Pedido = new Pedido(
-        this.formulario.value.endereco,
-        this.formulario.value.numero,
-        this.formulario.value.complemento,
-        this.formulario.value.formaPagamento,
-      );
 
-      this.ordemCompraService.efetivarCompra(pedido)
-        .subscribe((idPedido: number) => {
-          this.idPedidoCompra = idPedido;
-          console.log(this.idPedidoCompra);
-        });
+      if (this.carrinhoService.exibirItens().length === 0) {
+        alert('Você não selecionou nenhum item!');
+      } else {
+        let pedido: Pedido = new Pedido(
+          this.formulario.value.endereco,
+          this.formulario.value.numero,
+          this.formulario.value.complemento,
+          this.formulario.value.formaPagamento,
+        );
+  
+        this.ordemCompraService.efetivarCompra(pedido)
+          .subscribe((idPedido: number) => {
+            this.idPedidoCompra = idPedido;
+            console.log(this.idPedidoCompra);
+          });
+      }    
     }
   }
 
